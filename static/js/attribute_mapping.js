d6889e006d9fe632fc34a9a801264a6766f407e5
Akriti Agrawal <akritiagarwal76@gmail.com>
2026-06-22 12:53:32 +0530

    Populate attribute and visual property dropdowns from discovered graph data

--- static/js/attribute_mapping.js
+++ static/js/attribute_mapping.js
@@ -7,6 +7,20 @@ var attributeMapping = {
 
   discoveredAttributes: null,
 
+  // Which visual properties support which mapping types.
+  VISUAL_PROPERTIES: {
+    node: [
+      { id: 'background-color', label: 'Node color', discrete: true, continuous: true },
+      { id: 'width', label: 'Node size', discrete: false, continuous: true },
+      { id: 'shape', label: 'Node shape', discrete: true, continuous: false }
+    ],
+    edge: [
+      { id: 'line-color', label: 'Edge color', discrete: true, continuous: true },
+      { id: 'width', label: 'Edge width', discrete: false, continuous: true },
+      { id: 'line-style', label: 'Edge style', discrete: true, continuous: false }
+    ]
+  },
+
   init: function () {
     if ( typeof graph_json === 'undefined' || !graph_json ) {
       this.discoveredAttributes = { nodes: {}, edges: {} };
@@ -84,10 +98,137 @@ var attributeMapping = {
       e.preventDefault();
       attributeMapping.closePanel();
     } );
+
+    this.bindFormEvents();
+  },
+
+  bindFormEvents: function () {
+    // When element type (nodes / edges) changes, reset to discrete mapping
+    // and repopulate both dropdowns based on the new element type.
+    $( '#mappingElementType' ).off( 'change' ).on( 'change', function () {
+      $( '#mappingMappingType' ).val( 'discrete' );
+      attributeMapping.populateAttributeDropdown();
+      attributeMapping.populateVisualPropertyDropdown();
+    } );
+
+    // When attribute changes, infer mapping type (continuous vs discrete)
+    // from the discovered attribute meta and refresh visual properties.
+    $( '#mappingAttribute' ).off( 'change' ).on( 'change', function () {
+      attributeMapping.syncMappingTypeFromAttribute();
+      attributeMapping.populateVisualPropertyDropdown();
+    } );
+
+    // When mapping type is manually changed, just refresh visual properties.
+    $( '#mappingMappingType' ).off( 'change' ).on( 'change', function () {
+      attributeMapping.populateVisualPropertyDropdown();
+    } );
+  },
+
+  getSelectedElementType: function () {
+    var elementType = $( '#mappingElementType' ).val();
+    return elementType === 'edge' ? 'edge' : 'node';
+  },
+
+  getSelectedMappingType: function () {
+    return $( '#mappingMappingType' ).val() === 'continuous' ? 'continuous' : 'discrete';
+  },
+
+  getVisualPropertiesForSelection: function () {
+    var elementType = this.getSelectedElementType();
+    var mappingType = this.getSelectedMappingType();
+    var properties = this.VISUAL_PROPERTIES[ elementType ] || [];
+
+    return _.filter( properties, function ( property ) {
+      return mappingType === 'continuous' ? property.continuous : property.discrete;
+    } );
+  },
+
+  getAttributesForElementType: function ( elementType ) {
+    var discovered = this.discoveredAttributes || { nodes: {}, edges: {} };
+    return elementType === 'edge' ? discovered.edges : discovered.nodes;
+  },
+
+  getSelectedAttributeMeta: function () {
+    var attributeName = $( '#mappingAttribute' ).val();
+    if ( !attributeName ) {
+      return null;
+    }
+
+    var attributeMap = this.getAttributesForElementType( this.getSelectedElementType() );
+    return attributeMap[ attributeName ] || null;
+  },
+
+  syncMappingTypeFromAttribute: function () {
+    var meta = this.getSelectedAttributeMeta();
+    if ( !meta ) {
+      this.populateVisualPropertyDropdown();
+      return;
+    }
+
+    var mappingType = meta.type === 'numerical' ? 'continuous' : 'discrete';
+    $( '#mappingMappingType' ).val( mappingType );
+    this.populateVisualPropertyDropdown();
+  },
+
+  populateAttributeDropdown: function () {
+    var elementType = this.getSelectedElementType();
+    var attributeMap = this.getAttributesForElementType( elementType );
+    var attributeNames = _.sortBy( _.keys( attributeMap ) );
+    var $select = $( '#mappingAttribute' );
+
+    $select.empty();
+    $select.append( $( '<option>', {
+      value: '',
+      text: 'Select attribute...'
+    } ) );
+
+    if ( attributeNames.length === 0 ) {
+      $select.empty();
+      $select.append( $( '<option>', {
+        value: '',
+        text: 'No mappable attributes found'
+      } ) );
+      $select.prop( 'disabled', true );
+      return;
+    }
+
+    $select.prop( 'disabled', false );
+    _.each( attributeNames, function ( name ) {
+      $select.append( $( '<option>', {
+        value: name,
+        text: name
+      } ) );
+    } );
+  },
+
+  populateVisualPropertyDropdown: function () {
+    var properties = this.getVisualPropertiesForSelection();
+    var $select = $( '#mappingVisualProperty' );
+
+    $select.empty();
+    $select.append( $( '<option>', {
+      value: '',
+      text: 'Select visual property...'
+    } ) );
+
+    if ( properties.length === 0 ) {
+      $select.prop( 'disabled', true );
+      return;
+    }
+
+    $select.prop( 'disabled', false );
+    _.each( properties, function ( property ) {
+      $select.append( $( '<option>', {
+        value: property.id,
+        text: property.label
+      } ) );
+    } );
   },
 
   openPanel: function () {
     this.init();
+    this.populateAttributeDropdown();
+    this.populateVisualPropertyDropdown();
 
     $( '.gs-sidebar-nav' ).removeClass( 'active' );
     $( '#attributeMappingSideBar' ).addClass( 'active' );
